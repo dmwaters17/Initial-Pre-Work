23ce54f64571d5eebb747930cea58e0e09f24f43
David W <dmwaters17@gmail.com>
2019-06-16 19:30:00 -0600

    Added var myStr = "I am a \"double quoted\" string inside \"double quotes\".";

---
+++ 5-Introduction-to-JavaScript/22-Escaping-Literal-Quotes-in-Strings.js
@@ -0,0 +1,3 @@
+// In order to have quotation in a string without being confused as a beginning and end of a string - wrap the quoted remark in backslashes.
+
+var myStr = "I am a \"double quoted\" string inside \"double quotes\".";
