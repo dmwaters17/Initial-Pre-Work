ab9a872c9872ba43c0ad66c14645e8d102bcf7c3
David W <dmwaters17@gmail.com>
2019-06-16 19:31:29 -0600

    Changed var studlyCapVar; var properCamelCase; var titleCaseOver; studlyCapVar = 10; properCamelCase = "A String"; titleCaseOver = 9000;

---
+++ 5-Introduction-to-JavaScript/6-Understanding-Case-Sensitivity-in-Variables.js
@@ -0,0 +1,11 @@
+// JavaScript function names are case sensitive. It is common practice for a function name to start with the first word without capitals. The subsequent words start with capital letters followed by uncapitalized letters.
+
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
