b134c3b851b19a7a5358d04a73808f55b68b0af8
David W <dmwaters17@gmail.com>
2019-06-16 19:29:52 -0600

    Added var myFirstName = "David" var myLastName = 'Waters'

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,9 @@
+// Strings are defined by wrapping the value in double or single quotations. 
+
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+var myFirstName = "David"
+var myLastName = 'Waters'
