a8196c1f860779ab51385b757ebd1b59f5cfdb6e
David W <dmwaters17@gmail.com>
2019-06-16 19:30:51 -0600

    Added var a = 9;

---
+++ 5-Introduction-to-JavaScript/4-Initializing-Variables-with-the-Assignment-Operator.js
@@ -0,0 +1,7 @@
+// You can initialize a variable by using var and assign it a value using = in the same decleration.
+
+// Example
+var ourVar = 19;
+
+// Only change code below this line
+var a = 9;
