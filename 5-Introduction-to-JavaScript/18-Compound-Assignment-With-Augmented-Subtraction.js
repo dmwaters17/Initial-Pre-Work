45e69573fb202126df0b504fb49284514a86e467
David W <dmwaters17@gmail.com>
2019-06-16 19:29:16 -0600

    Added a -= 6; b -= 15; c -= 1;

---
+++ 5-Introduction-to-JavaScript/18-Compound-Assignment-With-Augmented-Subtraction.js
@@ -0,0 +1,11 @@
+// -= will modify and subtract a value from a variable at the same time. 
+
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Only modify code below this line
+
+a -= 6;
+b -= 15;
+c -= 1;
