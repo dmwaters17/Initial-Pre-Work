36e6158a11cfc6c5c7ab2ca7a7b2cd25fc81cbbc
David W <dmwaters17@gmail.com>
2019-06-16 19:30:23 -0600

    Added var myStr = "FirstLine\n\t\\SecondLine\nThirdLine";

---
+++ 5-Introduction-to-JavaScript/24-Escape-Sequences-in-Strings.js
@@ -0,0 +1,5 @@
+// Escape sequences are necessary to use quotations within a string as well as backslashes. There are also codes to use to manipulate the string - /n creates a new line and /t creates a tab. 
+
+var myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
+
+// Change this line
