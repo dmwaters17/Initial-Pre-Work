ce307b879a5e0646c59a999779d3e60272ab4420
David W <dmwaters17@gmail.com>
2019-06-16 19:29:33 -0600

    Added var myName;

---
+++ 5-Introduction-to-JavaScript/2-Declare-JavaScript-Variables.js
@@ -0,0 +1,7 @@
+/* Javascript has seven data types. Variables allow computers to point to the label of the data as opposed to the data itself. Variables can store different data at different times. Variables are defined with 'var'. Variables can contain numbers, letters, and special characters but may not start with a number or contain spaces. */
+// Example
+var ourName;
+
+// Declare myName below this line
+
+var myName;
