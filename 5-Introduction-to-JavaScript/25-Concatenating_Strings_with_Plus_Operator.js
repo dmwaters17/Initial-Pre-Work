760705668392eb451ee82fc47bb19e6ce00f1cb6
David W <dmwaters17@gmail.com>
2019-06-16 19:30:33 -0600

    Added var myStr = "This is the start. " + "This is the end.";

---
+++ 5-Introduction-to-JavaScript/25-Concatenating_Strings_with_Plus_Operator.js
@@ -0,0 +1,8 @@
+// You can concetenate strings by placing + between the two. Spaces will need to be p;aced within the strings if desired - concatenation does not create a space. 
+
+// Example
+var ourStr = "I come first. " + "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the start. " + "This is the end.";
