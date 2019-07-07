39bd2ed8aa79c3a1b6b89caf10133a648b9e184a
David W <dmwaters17@gmail.com>
2019-07-06 19:19:56 -0600

    Added
    if (val > 5) {
        result = "Bigger than 5";
      } else {
        result = "5 or Smaller";
      }

---
+++ 5-Introduction-to-JavaScript/68-Introducing_Else_Statements.js
@@ -0,0 +1,18 @@
+// An else statement can add a different outcome to an if statement should the if return false.
+
+function testElse(val) {
+  var result = "";
+  // Only change code below this line
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  } else {
+    result = "5 or Smaller";
+  }
+
+  // Only change code above this line
+  return result;
+}
+
+// Change this value to test
+testElse(4);
