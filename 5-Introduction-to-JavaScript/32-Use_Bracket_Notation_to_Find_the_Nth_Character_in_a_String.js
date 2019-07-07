988c1a56d3c9b2e33aac6476bfb57430dc101333
David W <dmwaters17@gmail.com>
2019-07-06 19:06:14 -0600

    Added
    var thirdLetterOfLastName = lastName[2];

---
+++ 5-Introduction-to-JavaScript/32-Use_Bracket_Notation_to_Find_the_Nth_Character_in_a_String.js
@@ -0,0 +1,11 @@
+// Bracket notation can index any character in a string. 
+
+// Example
+var firstName = "Ada";
+var secondLetterOfFirstName = firstName[1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var thirdLetterOfLastName = lastName[2];
