6f69a2f814a887997deb55158fd89f838682b03f
David W <dmwaters17@gmail.com>
2019-07-06 19:05:39 -0600

    Added
    firstLetterOfLastName = lastName[0];

---
+++ 5-Introduction-to-JavaScript/30-Use_Bracket_Notation_to_Find_the_First_Character_in_a_String.js
@@ -0,0 +1,14 @@
+// Bracket notation will return the character at the index indicated. Indexing begins at 0 as opposed to 1 as one would expect.
+
+// Example
+var firstLetterOfFirstName = "";
+var firstName = "Ada";
+
+firstLetterOfFirstName = firstName[0];
+
+// Setup
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0];
