31c35e24e895560716b85d9797b314e9e4ce448e
David W <dmwaters17@gmail.com>
2019-07-06 19:15:47 -0600

    Added
    return true;

---
+++ 5-Introduction-to-JavaScript/55-Understanding_Boolean_Values.js
@@ -0,0 +1,10 @@
+// JavaScript has the boolean data type which has an on or off option represented as true or false respectively.
+
+function welcomeToBooleans() {
+
+// Only change code below this line.
+
+return true; // Change this line
+
+// Only change code above this line.
+}
