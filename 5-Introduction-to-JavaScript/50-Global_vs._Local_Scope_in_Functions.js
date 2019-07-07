e646af873be8e70e94816312d3c3198e0f1b85ce
David W <dmwaters17@gmail.com>
2019-07-06 19:14:46 -0600

    Added
       var outerWear = "sweater";

---
+++ 5-Introduction-to-JavaScript/50-Global_vs._Local_Scope_in_Functions.js
@@ -0,0 +1,8 @@
+// When both local and global variables are present with differing values; the local variable will override the global.
+
+// Setup
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+   var outerWear = "sweater";
