127bf846cb4a82334e64b6eb38d41d81175dccd4
David W <dmwaters17@gmail.com>
2019-07-06 19:07:33 -0600

    var myArray = [["Nesting", "Arrays"],["Is","Fun"]];

---
+++ 5-Introduction-to-JavaScript/37-Nest_one_Array_within_Another_Array.js
@@ -0,0 +1,7 @@
+// Multi-dimensional arrays are arrays nested within other arrays.
+
+// Example
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Only change code below this line.
+var myArray = [["Nesting", "Arrays"],["Is","Fun"]];
