ddd426e2dc6b4e278a591ec6bacba0c5ed791e34
David W <dmwaters17@gmail.com>
2019-07-06 19:15:10 -0600

    Added
    var returnedValue = addFive();

---
+++ 5-Introduction-to-JavaScript/52-Understanding_Undefined_Value_returned_from_a_Function.js
@@ -0,0 +1,16 @@
+// Functions without return statements can alter variables if so declared. However, the function will be returned as undefined.
+
+// Example
+var sum = 0;
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive() {
+  sum = sum + 5;
+}
+
+
+// Only change code above this line
+var returnedValue = addFive();
