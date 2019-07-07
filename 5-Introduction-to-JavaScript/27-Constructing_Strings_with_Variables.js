3e27a6d1fa998dcd744c6b2f589c5449041f7d48
David W <dmwaters17@gmail.com>
2019-07-06 19:04:54 -0600

    Added
    var myName = "Dave";
    var myStr;"My name is " + myName + " and I am well!"

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,9 @@
+// You can place defined strings inbetween other strings using this format - + var +
+
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Dave";
+var myStr;"My name is " + myName + " and I am well!"
