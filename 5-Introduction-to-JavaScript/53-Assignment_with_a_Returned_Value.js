eaf775f55efb6256dd4bc4d859e3e82ec9dfb27c
David W <dmwaters17@gmail.com>
2019-07-06 19:15:21 -0600

    Added
    processed = processArg(7);

---
+++ 5-Introduction-to-JavaScript/53-Assignment_with_a_Returned_Value.js
@@ -0,0 +1,20 @@
+// Return values can be assigned to variables because values to the right if the = is resolved. 
+
+// Example
+var changed = 0;
+
+function change(num) {
+  return (num + 5) / 3;
+}
+
+changed = change(10);
+
+// Setup
+var processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+// Only change code below this line
+processed = processArg(7);
