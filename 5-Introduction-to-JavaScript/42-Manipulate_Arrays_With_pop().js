fe332fd7f82a28db29d76bda50d8962a63696656
David W <dmwaters17@gmail.com>
2019-07-06 19:11:56 -0600

    Added
    var removedFromMyArray = myArray.pop();

---
+++ 5-Introduction-to-JavaScript/42-Manipulate_Arrays_With_pop().js
@@ -0,0 +1,12 @@
+// The .pop() function removes the last item from an array and can then be assigned to another variable. 
+
+// Example
+var ourArray = [1,2,3];
+var removedFromOurArray = ourArray.pop();
+// removedFromOurArray now equals 3, and ourArray now equals [1,2]
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line.
+var removedFromMyArray = myArray.pop();
