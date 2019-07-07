67a752e2a9804de2533c712fb9df2adf97e967dd
David W <dmwaters17@gmail.com>
2019-07-06 19:18:37 -0600

    Added
    if (val < 25)
    
    if (val < 55)

---
+++ 5-Introduction-to-JavaScript/64-Comparison_with_the_Less_Than_Operator.js
@@ -0,0 +1,16 @@
+// The less than operator will return true if the value to the right is less than the value to the left.
+
+function testLessThan(val) {
+  if (val < 25) {  // Change this line
+    return "Under 25";
+  }
+
+  if (val < 55) {  // Change this line
+    return "Under 55";
+  }
+
+  return "55 or Over";
+}
+
+// Change this value to test
+testLessThan(10);
