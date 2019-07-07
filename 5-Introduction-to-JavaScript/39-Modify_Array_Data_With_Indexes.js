ebdb9e7846f8c91062416759afc5d5e36c9544a7
David W <dmwaters17@gmail.com>
2019-07-06 19:09:22 -0600

    Added
    myArray[0] = 45

---
+++ 5-Introduction-to-JavaScript/39-Modify_Array_Data_With_Indexes.js
@@ -0,0 +1,10 @@
+// Arrays are mutable, unlike strings, and can be substituted with other values.
+
+// Example
+var ourArray = [18,64,99];
+ourArray[1] = 45; // ourArray now equals [18,45,99].
+
+// Setup
+var myArray = [18,64,99];
+myArray[0] = 45
+// Only change code below this line.
