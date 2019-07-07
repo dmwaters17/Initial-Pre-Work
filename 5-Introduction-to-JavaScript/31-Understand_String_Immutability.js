641bf4d7e2e12fff2cacb1a360b0112bb694831d
David W <dmwaters17@gmail.com>
2019-07-06 19:05:55 -0600

    Added
    myStr = "Hello World";

---
+++ 5-Introduction-to-JavaScript/31-Understand_String_Immutability.js
@@ -0,0 +1,8 @@
+// Strings are immutable and cannot be changed. The variable must be reassigned a new string in order to alter the original string value of that variable.
+
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+
+myStr = "Hello World"; // Fix Me
