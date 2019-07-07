765b5ee61b1dbf1902a95110153834df9f360330
David W <dmwaters17@gmail.com>
2019-07-06 19:08:59 -0600

    Added
    var myData = myArray[0]

---
+++ 5-Introduction-to-JavaScript/38-Access_Array_Data_with_Indexes.js
@@ -0,0 +1,10 @@
+// Similar to how a character of a string can be returned using bracket notation, data in an array can be also be located using bracket notation.
+
+// Example
+var ourArray = [50,60,70];
+var ourData = ourArray[0]; // equals 50
+
+// Setup
+var myArray = [50,60,70];
+var myData = myArray[0]
+// Only change code below this line.
