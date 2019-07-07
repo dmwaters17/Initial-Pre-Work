ed945219dea56b1ae9d5f7d6a9d834f203ddaea2
David W <dmwaters17@gmail.com>
2019-07-06 19:17:49 -0600

    Added if (val > 100) {
    if (val > 10) {

---
+++ 5-Introduction-to-JavaScript/62-Comparison_with_the_Greater_Than_Operator.js
@@ -0,0 +1,16 @@
+// Another comparison operator is the greater than > operator. It will return true if the value to the right is greater than the value to the left.
+
+function testGreaterThan(val) {
+  if (val > 100) {  // Change this line
+    return "Over 100";
+  }
+
+  if (val > 10) {  // Change this line
+    return "Over 10";
+  }
+
+  return "10 or Under";
+}
+
+// Change this value to test
+testGreaterThan(10);
