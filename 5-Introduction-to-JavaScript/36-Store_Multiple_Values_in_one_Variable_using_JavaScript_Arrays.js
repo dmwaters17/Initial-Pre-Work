d7e7fb4f8b60fc9bd3a9847fb7c0dd1e29d55c95
David W <dmwaters17@gmail.com>
2019-07-06 19:07:19 -0600

    Added
    var myArray = ["string", 17, "Another", 12 , "String"];

---
+++ 5-Introduction-to-JavaScript/36-Store_Multiple_Values_in_one_Variable_using_JavaScript_Arrays.js
@@ -0,0 +1,7 @@
+// Arrays are several pieces of data in a single variable. They are created with square brackets on each end with commas separating each piece of data.
+
+// Example
+var ourArray = ["John", 23];
+
+// Only change code below this line.
+var myArray = ["string", 17, "Another", 12 , "String"];
