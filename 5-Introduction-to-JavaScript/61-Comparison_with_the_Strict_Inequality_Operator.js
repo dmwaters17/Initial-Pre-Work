9a9fc17564aff1b33bdc85f883713fa6369cb6a6
David W <dmwaters17@gmail.com>
2019-07-06 19:17:16 -0600

    Added
    if (val !== 17) {

---
+++ 5-Introduction-to-JavaScript/61-Comparison_with_the_Strict_Inequality_Operator.js
@@ -0,0 +1,17 @@
+// Just as equality has a strict counterpart, so too does inequality. Strict inequality will take into account the type of value.
+
+// Setup
+function testStrictNotEqual(val) {
+  // Only Change Code Below this Line
+
+  if (val !== 17) {
+
+  // Only Change Code Above this Line
+
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testStrictNotEqual(10);
