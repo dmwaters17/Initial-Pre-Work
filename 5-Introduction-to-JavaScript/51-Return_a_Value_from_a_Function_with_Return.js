65bceb56b79f2444d69a04b54c8b80710aa741f0
David W <dmwaters17@gmail.com>
2019-07-06 19:15:00 -0600

    Added function timesFive(num) {   return num * 5; }
    console.log(minusSeven(10));

---
+++ 5-Introduction-to-JavaScript/51-Return_a_Value_from_a_Function_with_Return.js
@@ -0,0 +1,14 @@
+// You can create a function with a parameter. Then add return parameter with an additional value. When the function is called with a variable it will return the variable along with the value.
+
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+function timesFive(num) {
+  return num * 5;
+}
+
+
+console.log(minusSeven(10));
