e77b4e094c88fd87a72cff50bb4677bfa6b3335f
David W <dmwaters17@gmail.com>
2019-07-06 19:12:48 -0600

    Added
    function reusableFunction(){
        console.log("Hi World")};
    
    reusableFunction();

---
+++ 5-Introduction-to-JavaScript/46-Write_Reusable_JavaScript_with_Functions.js
@@ -0,0 +1,14 @@
+//A Function can be created with a string and be called to print that string. 
+
+// Example
+function ourReusableFunction() {
+  console.log("Heyya, World");
+}
+
+ourReusableFunction();
+
+// Only change code below this line
+function reusableFunction(){
+    console.log("Hi World")};
+
+reusableFunction();
