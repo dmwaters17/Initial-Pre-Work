5cd6b06ec74bb661bc64b9158de656094f1d7689
David W <dmwaters17@gmail.com>
2019-07-06 19:03:44 -0600

    Added
    var myStr = "This is the first sentence. ";
    myStr += "This is the second sentence.";

---
+++ 5-Introduction-to-JavaScript/26-Concatenating_Strings_with_the_Plus_Equals_Operator.js
@@ -0,0 +1,10 @@
+// The += operator can be useful to concatenate long strings over multiple lines. 
+
+// Example
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
