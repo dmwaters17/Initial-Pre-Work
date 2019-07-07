301cb7b56336a0bcdd200be49a74f73f56971be1
David W <dmwaters17@gmail.com>
2019-07-06 19:12:16 -0600

    Added
    myArray.unshift(["Paul",35])

---
+++ 5-Introduction-to-JavaScript/44-Manipulate_Arrays_With_unshift().js
@@ -0,0 +1,14 @@
+// The unshift function will add an item or array to the beginning of an array.
+
+// Example
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.shift(); // ourArray now equals ["J", "cat"]
+ourArray.unshift("Happy");
+// ourArray now equals ["Happy", "J", "cat"]
+
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+
+// Only change code below this line.
+myArray.unshift(["Paul",35])
