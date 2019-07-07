5429bca875dec38bebfa48c497a300c5f7c3a7a2
David W <dmwaters17@gmail.com>
2019-07-06 19:13:00 -0600

    Added
    function functionWithArgs(a, b){
      console.log(a + b);
    }
    
    functionWithArgs(999, 111);

---
+++ 5-Introduction-to-JavaScript/47-Passing_Values_to_Functions_with_Arguments.js
@@ -0,0 +1,15 @@
+// Variables within functions that act as placeholders are parameters. When the parameters hold actual values they are called arguments. You can create a function with variables and then call it with any assortment of values. 
+
+// Example
+function ourFunctionWithArgs(a, b) {
+  console.log(a - b);
+}
+ourFunctionWithArgs(10, 5); // Outputs 5
+
+// Only change code below this line.
+
+function functionWithArgs(a, b){
+  console.log(a + b);
+}
+
+functionWithArgs(999, 111);
