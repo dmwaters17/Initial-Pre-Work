ca67081e4f5ccc7649aaaf7b43d1a02cecb6c83f
David W <dmwaters17@gmail.com>
2019-07-06 19:05:08 -0600

    Added
    var someAdjective = "fantastic";
    var myStr = "Learning to code is ";
    myStr += someAdjective

---
+++ 5-Introduction-to-JavaScript/28-Appending_Variables_to_Strings.js
@@ -0,0 +1,12 @@
+// += can also be used to append a string to another string by placing bewtween the two string variables. 
+
+// Example
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+
+// Only change code below this line
+
+var someAdjective = "fantastic";
+var myStr = "Learning to code is ";
+myStr += someAdjective
