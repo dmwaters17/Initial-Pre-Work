bc74b5d9b9d99b12468a9a7b40f3d49a643b29e8
David W <dmwaters17@gmail.com>
2019-07-06 19:06:38 -0600

    var secondToLastLetterOfLastName = lastName[lastName.length -2];

---
+++ 5-Introduction-to-JavaScript/34-Use_Bracket_Notation_to_Find_the_Nth-to-Last_Character_in_a_String.js
@@ -0,0 +1,11 @@
+// You can subtract any value from the length in order to find that values distance from the last character. 
+
+// Example
+var firstName = "Ada";
+var thirdToLastLetterOfFirstName = firstName[firstName.length - 3];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var secondToLastLetterOfLastName = lastName[lastName.length -2];
