75808a583e70981220910d4248b1e8e129bff1f3
David W <dmwaters17@gmail.com>
2019-07-06 19:16:13 -0600

    Added
    function testEqual(val) {
      if (val == 12) {
        return "Equal";
      }
      return "Not Equal";
    }v

---
+++ 5-Introduction-to-JavaScript/57-Comparison_with_the_Equality_Operator.js
@@ -0,0 +1,12 @@
+// Comparison operators use boolean logic to return certain values given the corrrect conditions. The equals operator will return true if two values match and false if they do not match. 
+
+// Setup
+function testEqual(val) {
+  if (val == 12) {
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testEqual(12);
