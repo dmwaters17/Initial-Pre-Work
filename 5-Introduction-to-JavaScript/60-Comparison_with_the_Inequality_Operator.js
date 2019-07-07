66582bcd87246a80491c29d579a4701de3e71322
David W <dmwaters17@gmail.com>
2019-07-06 19:17:05 -0600

    Added
    function testNotEqual(val) {
      if (val != 99) { // Change this line
        return "Not Equal";
      }
      return "Equal";
    }

---
+++ 5-Introduction-to-JavaScript/60-Comparison_with_the_Inequality_Operator.js
@@ -0,0 +1,12 @@
+// The inequality operator will return true when values do not equal each other and false when they do. It is the opposite of the equality operator.
+
+// Setup
+function testNotEqual(val) {
+  if (val != 99) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testNotEqual(10);
