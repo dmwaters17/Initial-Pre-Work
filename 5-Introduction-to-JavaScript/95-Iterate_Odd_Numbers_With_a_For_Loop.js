5ffb300c9a138b224a4b42bb789fe316415985e0
David W <dmwaters17@gmail.com>
2019-07-13 19:08:53 -0600

    Added
    for (var i = 1; i < 10; i += 2) {
      myArray.push(i);

---
+++ 5-Introduction-to-JavaScript/95-Iterate_Odd_Numbers_With_a_For_Loop.js
@@ -0,0 +1,16 @@
+// By changing incrementation in the final expression, numbers can be pushed by increments of whatever number.
+
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 10; i += 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+for (var i = 1; i < 10; i += 2) {
+  myArray.push(i);
+}
+// Only change code below this line.
