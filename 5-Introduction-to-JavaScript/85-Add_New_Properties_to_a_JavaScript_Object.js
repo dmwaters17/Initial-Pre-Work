89d6e2c3f17f2b65ec94a1fec63d34341cb7a973
David W <dmwaters17@gmail.com>
2019-07-13 19:02:56 -0600

    Added
    myDog.bark = "Ruff"

---
+++ 5-Introduction-to-JavaScript/85-Add_New_Properties_to_a_JavaScript_Object.js
@@ -0,0 +1,22 @@
+// Adding a property to an object is very similar to modifying properties. Instead of having a pre existing property with dot or bracket notation, add a new property that does not already exist.
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.bark = "bow-wow";
+
+// Setup
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line.
+myDog.bark = "Ruff"
