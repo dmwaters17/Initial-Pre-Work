bb25f9aced2b38ec6a0b681e1cb1524a39fd719e
David W <dmwaters17@gmail.com>
2019-07-13 18:59:32 -0600

    Added
      return (a == b ? true : false);

---
+++ 5-Introduction-to-JavaScript/106-Use_the_Conditional_(Ternary)_Operator.js
@@ -0,0 +1,7 @@
+// The ternary operator functions as a single line if/else statement and can be used to compare values to return true or false. 
+
+function checkEqual(a, b) {
+  return (a == b ? true : false);
+}
+
+checkEqual(1, 2);
