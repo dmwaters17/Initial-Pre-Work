d4cf237aab3be7fd5cccb7c07446540119fe04ec
David W <dmwaters17@gmail.com>
2019-07-13 19:03:26 -0600

    Added
    delete myDog.tails;

---
+++ 5-Introduction-to-JavaScript/86-Delete_Properties_from_a_JavaScript_Object.js
@@ -0,0 +1,24 @@
+// Just as you would add or modify a property to an object using dot or bracket notation, precede the statement with delete to delete a property.
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"],
+  "bark": "bow-wow"
+};
+
+delete ourDog.bark;
+
+// Setup
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+
+// Only change code below this line.
+delete myDog.tails;
