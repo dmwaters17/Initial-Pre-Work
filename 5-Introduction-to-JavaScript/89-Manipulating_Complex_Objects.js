9fea01e387bc74c2f47d606064b8078092841619
David W <dmwaters17@gmail.com>
2019-07-13 19:04:10 -0600

    Added
      {
        "artist": "Pink Floyd",
        "title": "The Wall",
        "release_year": 1979,
        "formats": [
          "CD",
          "Cassette"
        ],
      }

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,26 @@
+// Objects can store flexible data, that is data that allows for differing types of values to be combined with one another. Here I am adding objects within an array, each object then containing properties and nested arrays. 
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  // Add record here
+
+  {
+    "artist": "Pink Floyd",
+    "title": "The Wall",
+    "release_year": 1979,
+    "formats": [
+      "CD",
+      "Cassette"
+    ],
+  }
+];
