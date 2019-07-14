41898b0d81233b0be7baedfbf29a0aa653341e15
David W <dmwaters17@gmail.com>
2019-07-13 19:01:03 -0600

    Added
       return a < b;

---
+++ 5-Introduction-to-JavaScript/77-Returning_Boolean_Values_from_Functions.js
@@ -0,0 +1,9 @@
+// Comparison operators use boolean logic, therefore if / else statements with true or false returns are not necessary.  Simply comparing the values will return a true or false.
+
+function isLess(a, b) {
+  // Fix this code
+   return a < b;
+}
+
+// Change these values to test
+isLess(10, 15);
