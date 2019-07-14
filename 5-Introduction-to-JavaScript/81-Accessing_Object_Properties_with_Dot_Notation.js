2505116e2996cd59c6b3c80e0dfae2f0ffe11cbe
David W <dmwaters17@gmail.com>
2019-07-13 19:02:03 -0600

    Added
    var hatValue = testObj.hat;
    var shirtValue = testObj.shirt;

---
+++ 5-Introduction-to-JavaScript/81-Accessing_Object_Properties_with_Dot_Notation.js
@@ -0,0 +1,13 @@
+// Once properties are assigned data, dot notation can be used to access the data. Recalling the object followed by .property will return the value.
+
+// Setup
+var testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+
+var hatValue = testObj.hat;      // Change this line
+var shirtValue = testObj.shirt;    // Change this line
