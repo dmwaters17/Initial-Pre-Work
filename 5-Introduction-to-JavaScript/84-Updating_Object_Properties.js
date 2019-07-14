a6236648c0b896aa80ae93dc30cd29d8c96872f5
David W <dmwaters17@gmail.com>
2019-07-13 19:02:47 -0600

    Added
    myDog.name = "Happy Coder"

---
+++ 5-Introduction-to-JavaScript/84-Updating_Object_Properties.js
@@ -0,0 +1,23 @@
+// Properties' values can be updated by using dot or bracket notation. Simply reassign a value to object.property = ...
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.name = "Happy Camper";
+
+// Setup
+var myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line.
+
+myDog.name = "Happy Coder"
