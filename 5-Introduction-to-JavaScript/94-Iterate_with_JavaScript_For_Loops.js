1c2e175e71602666dec11e30f8c697df918689b4
David W <dmwaters17@gmail.com>
2019-07-13 19:08:42 -0600

    Added
    for (var n = 1; n < 6; n++) {
      myArray.push(n);
    }

---
+++ 5-Introduction-to-JavaScript/94-Iterate_with_JavaScript_For_Loops.js
@@ -0,0 +1,16 @@
+// For loops are comprised of three expressions: initialization, condition, final-expression, each separated by a semicolon. For loops will run for the specified number of times. Values can be pushed to an array by setting a value in initialization, setting the condition in condition, and incrementing in final-expression.  
+
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for (var n = 1; n < 6; n++) {
+  myArray.push(n);
+}
