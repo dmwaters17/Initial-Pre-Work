0592456e9a1c9e869991b8de72356a87a74796fe
David W <dmwaters17@gmail.com>
2019-07-13 18:59:12 -0600

    Added
    return parseInt(str);

---
+++ 5-Introduction-to-JavaScript/104-Use_the_parseInt_Function.js
@@ -0,0 +1,7 @@
+// parseInt() function will convert a string into a number. It will return NaN if the string can be converted.
+
+function convertToInteger(str) {
+return parseInt(str);
+}
+
+convertToInteger("56");
