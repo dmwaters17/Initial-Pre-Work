722e5b59b87e51b7e9b5f6458285dbd113fcf4fb
David W <dmwaters17@gmail.com>
2019-07-13 19:04:20 -0600

    Added
    var secondTree = myPlants[1].list[1]; // Change this line

---
+++ 5-Introduction-to-JavaScript/90-Accessing_Nested_Objects.js
@@ -0,0 +1,16 @@
+// Dot and bracket notation can be used to access complex data that may be nested.
+
+// Setup
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+var gloveBoxContents = myStorage.car.inside["glove box"]; // Change this line
