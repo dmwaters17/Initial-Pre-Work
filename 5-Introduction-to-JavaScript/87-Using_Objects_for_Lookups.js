d811426feb4745d838fe703897895887e7b67a26
David W <dmwaters17@gmail.com>
2019-07-13 19:03:42 -0600

    Added
       var lookup = {
        "alpha" : "Adams",
        "bravo" : "Boston",
        "charlie" : "Chicago",
        "delta" : "Denver",
        "echo" : "Easy",
        "foxtrot" : "Frank"
      };
        result = lookup[val];

---
+++ 5-Introduction-to-JavaScript/87-Using_Objects_for_Lookups.js
@@ -0,0 +1,24 @@
+// In certain situations, objects can be used to lookup data more efficiently than switch or else/if statements.
+
+// Setup
+function phoneticLookup(val) {
+  var result = "";
+
+  // Only change code below this line
+   var lookup = {
+    "alpha" : "Adams",
+    "bravo" : "Boston",
+    "charlie" : "Chicago",
+    "delta" : "Denver",
+    "echo" : "Easy",
+    "foxtrot" : "Frank"
+  };
+    result = lookup[val];
+
+  // Only change code above this line
+  return result;
+
+}
+
+// Change this value to test
+phoneticLookup("delta");
