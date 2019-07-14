2c5543fce6c6fb19f0f44a0ebc6290792c2a14f5
David W <dmwaters17@gmail.com>
2019-07-13 19:07:23 -0600

    Added
    var secondTree = myPlants[1].list[1];

---
+++ 5-Introduction-to-JavaScript/91-Accessing_Nested_Arrays.js
@@ -0,0 +1,25 @@
+// Objects and arrays can be nested within objects. To access nested objects dot and bracket notations are used. to access arrays, bracket notation is used. Combining both dot and bracket notation allows for arrays to be accessed within objects.
+
+// Setup
+var myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+// Only change code below this line
+
+var secondTree = myPlants[1].list[1]; // Change this line
