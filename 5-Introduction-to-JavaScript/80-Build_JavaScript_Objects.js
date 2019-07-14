27026d14951d7812639abdd14a383111b08a6ea9
David W <dmwaters17@gmail.com>
2019-07-13 19:01:44 -0600

    Added
    var myDog = {
      "name": "Tuppence",
      "legs": 4,
      "tails": 1,
      "friends": ["FEWD!"]

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,22 @@
+// Objects use properties to store data. Each object can be assigned a property and value with that property. Properties can be strings or numbers.
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name": "Tuppence",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["FEWD!"]
+
+
+
+
+};
